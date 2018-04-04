cd4a7e195a374b6c8c1361ab9e96c8da51ab2d4c
Saurav <sp.gharti@gmail.com>
2018-04-04 18:52:48 +0000

    add package spirit

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'spirit', 'url': 'http://pypi.python.org/pypi/spirit/1.7.0'},
 	{'name':'askbot', 'url': 'http://pypi.python.org/pypi/askbot/0.10.2'},
 	{'name':'django-guardian', 'url': 'http://pypi.python.org/pypi/django-guardian/1.4.9'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
