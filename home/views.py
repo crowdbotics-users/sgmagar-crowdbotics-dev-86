9e16b15651844dd45a70701a77ac19cb6ab8d6c1
Saurav <sp.gharti@gmail.com>
2018-04-04 18:52:44 +0000

    add package askbot

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'askbot', 'url': 'http://pypi.python.org/pypi/askbot/0.10.2'},
 	{'name':'django-guardian', 'url': 'http://pypi.python.org/pypi/django-guardian/1.4.9'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
     ]
